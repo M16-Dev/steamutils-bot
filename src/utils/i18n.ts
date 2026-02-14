6c5ef725b721770615b522097041ae60328aebf7
Kameq <90651206+kameqdev@users.noreply.github.com>
2026-02-14 09:29:27 +0100

    feat: add i18n utility for internationalization

---
+++ src/utils/i18n.ts
@@ -0,0 +1,16 @@
+import i18next from "@i18next/i18next";
+import pl from "../locales/pl.json" with { type: "json" };
+import en from "../locales/en.json" with { type: "json" };
+
+await i18next.init({
+    fallbackLng: "en",
+    resources: {
+        en: { translation: en },
+        pl: { translation: pl },
+    },
+});
+
+export function t(key: string, locale: string, variables = {}) {
+    const lang = locale.split("-")[0];
+    return i18next.t(key, { lng: lang, ...variables });
+}
